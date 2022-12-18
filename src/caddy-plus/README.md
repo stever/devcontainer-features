b01b58753b43ec0546d660edbc0cda28412596fe
Steven Robertson <stever@hey.com>
2022-12-18 15:42:32 +0000

    Adding the caddy-plus feature

---
+++ src/caddy-plus/README.md
@@ -0,0 +1,14 @@
+
+# Caddy Plus (caddy-plus)
+
+Caddy plus additional extensions.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/stever/devcontainers-features/caddy-plus:1": {
+        "version": "latest"
+    }
+}
+```
---
+++ src/caddy-plus/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'caddy-plus'"
+
+# Install the xcaddy tool
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | \
+  sudo gpg --dearmor -o /usr/share/keyrings/caddy-xcaddy-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | \
+  sudo tee /etc/apt/sources.list.d/caddy-xcaddy.list
+sudo apt update
+sudo apt install xcaddy
+
+# Build caddy using xcaddy
+xcaddy build --with github.com/greenpau/caddy-security
+
+# Move caddy to bin
+mkdir -p ~/bin
+mv caddy ~/bin
+
+# Ensure bin is on path
+[[ ":$PATH:" != *":${HOME}/bin:"* ]] && PATH="${HOME}/bin:${PATH}"
